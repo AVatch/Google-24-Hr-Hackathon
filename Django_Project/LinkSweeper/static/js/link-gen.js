d1b5cc7db36940925ccc576edf85d1e6e7e86d2c
joeyuan19 <joe.yuan19@gmail.com>
2014-02-07 21:05:29 -0500

    started link-gen.js

--- Django_Project/LinkSweeper/static/js/link-gen.js
+++ Django_Project/LinkSweeper/static/js/link-gen.js
@@ -1,4 +1,52 @@
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+ * link-gen.js
+ * Javascript to generate damaged and dubious links
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  generateLink(level)
+ *      Arguments:
+ *          level - level the user is on 
+ *      Description:
+ *        - Wrapper to create a dubious link within the selected
+ *          elements
+ *      Usage:
+ *          $(selector).generate()
+ *
+ *  $.fn.generate - jQuery wrapper
+ *      Arguments:
+ *          None
+ *      Description:
+ *        - Wrapper to create a dubious link within the selected
+ *          elements
+ *      Usage:
+ *          $(selector).generate()
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
+
+/* Main Generation Function */
+function generateLink(level,probability_threshold) {
+}
+
+/* Individual Link Destruction Algorithms */
+function letterSwap(difficulty) {
+}
+
+
+/* Helper Methods */
+function levelToDifficulty(level) {
+    return 1.0 - 1.0/level; 
+}  
+
+
+/* jQuery wrapper */
+$.fn.generate = function() {
+    var difficulty = window.difficulty;
+    var link = generateLink(difficulty);
+    $(this).text(link); 
+}
 
 
